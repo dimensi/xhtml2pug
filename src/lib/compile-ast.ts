7a304e230eca3af9149390e7a47f881a6a8ec0c5
Nikita Nafranets <root@dimensi.dev>
2021-01-29 02:00:37 +0300

    a little refactoring

--- src/lib/compile-ast.ts
+++ src/lib/compile-ast.ts
@@ -41,77 +41,84 @@ const getNodesWithoutText = (nodes: readonly Nodes[]) => {
   return nodes;
 };
 
+type IndentOptions = {
+  readonly level: number;
+  readonly symbol: string;
+};
+
+const getIndent = ({ level, symbol }: IndentOptions) => symbol.repeat(level);
+
+const wrapText = (str: string, options) =>
+  str
+    ? '.\n' +
+      str
+        .trim()
+        .split('\n')
+        .map(str => getIndent(options) + str.trim())
+        .join('\n')
+    : '';
+
+const compileAttrs = (attrs: readonly Attr[]) =>
+  attrs
+    .map(({ key, value }) => [key, value ? `"${value}"` : null].filter(Boolean).join('='))
+    .join(' ');
+
+const compileDoctype = (node: Doctype, options: IndentOptions) =>
+  `${getIndent(options)}doctype ${compileAttrs(node.attrs)}`;
+
+const compileText = (node: Text, options: IndentOptions) =>
+  node.value.includes('\n')
+    ? '\n' +
+      node.value
+        .split('\n')
+        .map(str => `${getIndent(options)}| ${str.trim()}`)
+        .join('\n')
+    : ' ' + node.value;
+
+const compileScript = (node: Script, options: IndentOptions) =>
+  `${getIndent(options)}script${wrapAttrs(compileAttrs(node.attrs))}${wrapText(
+    node.value ?? '',
+    { ...options, level: options.level + 1 }
+  )}`;
+
+const compileStyle = (node: Style, options: IndentOptions) =>
+  `${getIndent(options)}style${wrapAttrs(compileAttrs(node.attrs))}${wrapText(
+    node.value ?? '',
+    { ...options, level: options.level + 1 }
+  )}`;
+
+const compileTag = (node: Tag, options: IndentOptions) => {
+  const { attrs, className, id } = formatAttrsForTag(node.attrs);
+  const tag = [
+    getIndent(options),
+    id || (className && node.name === 'div') ? '' : node.name,
+    id ? `#${id}` : '',
+    className ? '.' + className.split(' ').join('.') : '',
+    wrapAttrs(compileAttrs(attrs)),
+  ]
+    .filter(Boolean)
+    .join('');
+  const textNode = getFirstText(node.children);
+  if (!textNode) return tag;
+  return `${tag}${compileText(textNode, { ...options, level: options.level + 1 })}`;
+};
+
 export function compileAst(ast: readonly Nodes[]): string {
-  const getIndent = (level: number, symbol = '  ') => symbol.repeat(level);
-
-  const wrapText = (str: string, level: number) =>
-    str
-      ? '.\n' +
-        str
-          .trim()
-          .split('\n')
-          .map(str => getIndent(level) + str.trim())
-          .join('\n')
-      : '';
-
-  const compileAttrs = (attrs: readonly Attr[]) =>
-    attrs
-      .map(({ key, value }) => [key, value ? `"${value}"` : null].filter(Boolean).join('='))
-      .join(' ');
-
-  const compileDoctype = (node: Doctype, level: number) =>
-    `${getIndent(level)}doctype ${compileAttrs(node.attrs)}`;
-
-  const compileText = (node: Text, level: number) =>
-    node.value.includes('\n')
-      ? '\n' +
-        node.value
-          .split('\n')
-          .map(str => `${getIndent(level)}| ${str.trim()}`)
-          .join('\n')
-      : ' ' + node.value;
-  const compileScript = (node: Script, level: number) =>
-    `${getIndent(level)}script${wrapAttrs(compileAttrs(node.attrs))}${wrapText(
-      node.value ?? '',
-      level + 1
-    )}`;
-
-  const compileStyle = (node: Style, level: number) =>
-    `${getIndent(level)}style${wrapAttrs(compileAttrs(node.attrs))}${wrapText(
-      node.value ?? '',
-      level + 1
-    )}`;
-
-  const compileTag = (node: Tag, level: number) => {
-    const { attrs, className, id } = formatAttrsForTag(node.attrs);
-    const tag = [
-      getIndent(level),
-      id || className && node.name === 'div' ? '' : node.name,
-      id ? `#${id}` : '',
-      className ? '.' + className.split(' ').join('.') : '',
-      wrapAttrs(compileAttrs(attrs)),
-    ]
-      .filter(Boolean)
-      .join('');
-    const textNode = getFirstText(node.children);
-    if (!textNode) return tag;
-    return `${tag}${compileText(textNode, level + 1)}`;
-  };
-
+  const symbol = '  ';
   const deepCompile = (ast: readonly Nodes[], level = 0) =>
     ast.reduce<readonly string[]>((acc, node) => {
       switch (node.node) {
         case Node.Doctype:
-          return acc.concat(compileDoctype(node, level));
+          return acc.concat(compileDoctype(node, { level, symbol }));
         case Node.Script:
-          return acc.concat(compileScript(node, level));
+          return acc.concat(compileScript(node, { level, symbol }));
         case Node.Style:
-          return acc.concat(compileStyle(node, level));
+          return acc.concat(compileStyle(node, { level, symbol }));
         case Node.Text:
-          return acc.concat(compileText(node, level));
+          return acc.concat(compileText(node, { level, symbol }));
         case Node.Tag:
           return acc.concat(
-            compileTag(node, level),
+            compileTag(node, { level, symbol }),
             ...deepCompile(getNodesWithoutText(node.children), level + 1)
           );
       }
